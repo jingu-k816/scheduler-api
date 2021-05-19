548ae7e54d31cde600c6d64488ac774c143dc747
Jingu Kang <jinguk0816@gmail.com>
2021-05-19 19:01:20 -0400

    console log node env

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,13 @@
 const pg = require("pg");
-
+console.log("THIS IS THE NODE ENV",process.env.NODE_ENV);
 const client = new pg.Client({
   connectionString: process.env.DATABASE_URL || "",
   // ssl: {
   //   rejectUnauthorized: false,
   // },
-  ssl: {
-    rejectUnauthorized: process.env.DATABASE_URL ? true : false
-  }
-    
+  ssl: process.env.DATABASE_URL ? true : false
   
 });
-
 client
   .connect()
   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
