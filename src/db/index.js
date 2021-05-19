487aa4823f370954e5476b3b00b4fa846fd2e7e4
Jingu Kang <jinguk0816@gmail.com>
2021-05-19 19:06:39 -0400

    configure for both m1 and heroku deployment

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,24 @@
 const pg = require("pg");
-console.log("THIS IS THE NODE ENV",process.env.NODE_ENV);
-const client = new pg.Client({
+
+let client = new pg.Client({
   connectionString: process.env.DATABASE_URL || "",
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
   ssl: process.env.DATABASE_URL ? true : false
   
 });
+
+if (process.env.NODE_ENV === "production") {
+  client = new pg.Client({
+    connectionString: process.env.DATABASE_URL || "",
+    ssl: {
+      rejectUnauthorized: false,
+    }
+    //ssl: process.env.DATABASE_URL ? true : false
+    
+  });
+  
+} 
+
+
 client
   .connect()
   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
